d74cf13062a8e9a79b2963aaab70f2a91b32f8aa
Alejandro Giraldo <alejandro@giraldopineda.com>
2024-05-17 14:55:28 -0500

    feature: Setting SSR for blog posts
    I was doing page rendering without SSR which is ok for non static pages, but not for blogs and content pages.
    It also demonstrated some Astro routing features that I wanted to share with the viewers.
    Now we have and understand both methods, which give us the flexibility on the hybrid output in Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import { SITE_URL } from './src/consts'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://pricode.local',
+	site: SITE_URL,
 	integrations: [mdx(), sitemap()],
-	output: 'hybrid',
-	alejo : 'alejogiraldo'
+	output: 'hybrid'
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const GRAPHQL_URL = 'http://pricode.local/graphql'
+export const GRAPHQL_URL = 'http://pricode.local/graphql';
+export const SITE_URL = 'http://pricode.local/';
